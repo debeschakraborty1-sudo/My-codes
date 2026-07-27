7a79dbdb3bde10977221a4147816c08cb017478d
debeschakraborty1-sudo <debeschakraborty1@gmail.com>
2026-07-27 11:34:50 +0530

    Add files via upload

---
+++ checking the number.py
@@ -0,0 +1,22 @@
+'''num=int(input("enter value of num:"))
+
+if num>0:
+   print("the given number is psitive")
+
+age=int(input("enter the age:"))
+if age>18:
+    print("you are eligible to vote")
+     
+num=int(input("enter the number:"))
+if num%2==0:
+    print("the given number is even") 
+else:
+  print("the given number is odd")'''
+
+num=int(input("enter a the number:"))
+
+if num > 0:
+    print("the given number is positive")
+else:
+    print("the given number is negative")
+       
